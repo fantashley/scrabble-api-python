a8180e3afc83fbf56b708c790eafafa3c0ed7bd2
Ashley Nelson <amn@fantashley.com>
2019-06-30 15:40:31 -0500

    Apply PEP8 formatting

--- board_game.py
+++ board_game.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 
+
 class BoardGame:
 
     # Board coordinates
@@ -36,10 +37,13 @@ class BoardGame:
 
             for y, x in self.SQUARE_COORDS[square_type]:
                 # Quadrant 1
-                self.board[y][self.BOARD_X - 1 - x] = self.Square(None, square_type)
+                self.board[y][self.BOARD_X - 1 - x] = self.Square(None,
+                                                                  square_type)
                 # Quadrant 2
                 self.board[y][x] = self.Square(None, square_type)
                 # Quadrant 3
-                self.board[self.BOARD_Y - 1 - y][x] = self.Square(None, square_type)
+                self.board[self.BOARD_Y - 1 - y][x] = self.Square(None,
+                                                                  square_type)
                 # Quadrant 4
-                self.board[self.BOARD_Y - 1 - y][self.BOARD_X - 1 - x] = self.Square(None, square_type)
+                self.board[self.BOARD_Y - 1 - y][self.BOARD_X - 1 - x] = \
+                    self.Square(None, square_type)
