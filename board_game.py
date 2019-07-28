6d411b6c9a1f1362e45265e6e55f75d1704a4066
Ashley Nelson <amn@fantashley.com>
2019-07-28 11:41:02 -0500

    Updates to game logic

--- board_game.py
+++ board_game.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import numpy as np
 import random
 
 
@@ -23,6 +24,7 @@ class BoardGame:
         self.board = []
         self.players = []
         self.tile_bag = []
+        self.first_turn = True
 
         self.__populate_board()
         self.__initialize_players()
@@ -57,7 +59,7 @@ class BoardGame:
                     self.Square(None, square_type)
 
     def __initialize_players(self):
-    
+
         for player in range(self.player_count):
             tile_set = []
             self.players.append(tile_set)
@@ -73,3 +75,59 @@ class BoardGame:
         for player in self.players:
             for i in range(self.INITIAL_TILE_COUNT):
                 player.append(self.tile_bag.pop())
+
+    def place_tiles(self, tiles):
+        min_x = self.BOARD_X
+        max_x = -1
+        min_y = self.BOARD_Y
+        max_y = -1
+        for tile in tiles:
+            if self.board[tile.y][tile.x].tile is None:
+                self.board[tile.y][tile.x].tile = tile.letter[0]
+                min_x = min(tile.x, min_x)
+                min_y = min(tile.y, min_y)
+                max_x = max(tile.x, max_x)
+                max_y = max(tile.y, max_y)
+            else:
+                return False
+        # Find word in direction of tile placement
+        start_x, start_y, end_x, end_y = \
+            __find_word(tiles[0].x, tiles[0].y, x_axis=int(min_x == max_x), \
+                        y_axis=int(min_y == max_y))
+        
+        
+
+        
+    def __is_valid_word()
+    def __find_word(self, x_coord, y_coord, x_axis=0, y_axis=0):
+        # Find beginning of word
+        cur_x = x_coord
+        cur_y = y_coord
+        
+        while cur_x >= 0 and cur_y >= 0:
+            if self.board[cur_y][cur_x].tile is not None:
+                cur_x -= x_axis
+                cur_y -= y_axis
+            else:
+                break
+        
+        start_x = cur_x + 1 * x_axis
+        start_y = cur_y + 1 * y_axis
+
+        # Find ending of word
+        cur_x = x_coord
+        cur_y = y_coord
+
+        while cur_x < self.BOARD_X and cur_y < self.BOARD_Y:
+            if self.board[cur_y][cur_x].tile is not None:
+                cur_x += x_axis
+                cur_y += y_axis
+            else:
+                break
+
+        end_x = cur_x - 1 * x_axis
+        end_y = cur_y - 1 * y_axis
+
+        np_array = np.array(self.board)
+
+        return np_array[start_y:(end_y + 1), start_x:(end_x + 1)]
